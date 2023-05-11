97dc29cab3e17efe7fd54000014dd1c82017242d
19jkunzler <jacobkunzler01@gmail.com>
2023-05-11 23:26:17 +0000

    created objects to import download files. As well as generate presigend URLs

---
+++ Projects/New Folder/boto3_s3/list_objects.py
@@ -0,0 +1,30 @@
+import boto3
+
+def filter_objects_extension(client, bucket, extension):
+    keys = []
+    response = client.list_objects_v2(Bucket=bucket)
+    for content in response["Contents"]:
+        if(extension in content["Key"][-(len(extension)):]):
+            keys.append(content["Key"])   
+    
+    return keys
+    
+    
+def list_object_keys(client, bucket, prefix=""):
+    keys = []
+    response = client.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    for content in response["Contents"]:
+        keys.append(content["Key"])   
+    
+    return keys
+
+if __name__== '__main__':
+    s3 = boto3.client('s3')
+#to search specific folders add the Prefix=path to search certain folders
+
+
+    response = list_object_keys(s3, 'kunzler-boto3-05112023', "test_text_string")
+    print(response)
+
+    response = filter_objects_extension(s3, 'kunzler-boto3-05112023', ".txt")
+    print(response)
